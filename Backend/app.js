8d1d9408d09f21f65dd614df10a00961874cb99f
Prashanth Naroju <prashanth.naro@gmail.com>
2024-08-08 12:37:08 +0530

    add the add-product page for get response and new route

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,12 @@ const express=require('express');
 const app=express();
 
 
+app.get("/add-product",(req,res,next)=>{
+    console.log("add product page rendered");
+    res.send("<h1>This is the add product page");
+})
+
+
 app.use((req,res,next)=>{
     console.log("server just started");
     res.send("<h1>Welcome to Express server");
