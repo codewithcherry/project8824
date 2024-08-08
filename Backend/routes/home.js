a3a2c62616a547c86df8762fedccc91dcc49885e
Prashanth Naroju <prashanth.naro@gmail.com>
2024-08-08 14:47:49 +0530

    code the homepage router code for the home route

--- Backend/routes/home.js
+++ Backend/routes/home.js
@@ -1,1 +1,16 @@
 // this route is for home page of the website
+
+const express=require('express');
+
+const path=require('path')
+
+const router=express.Router();
+
+const rootdir=require('../utils/utilpath');
+
+router.get("/",(req,res,next)=>{
+    console.logA("Home page");
+    res.sendFile(path.join(rootdir,'views','home.html'));
+})
+
+module.exports=router
