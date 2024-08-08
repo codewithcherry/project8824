8dd7c56dc876cdf75eb3619fde3144c8bf283638
Prashanth Naroju <prashanth.naro@gmail.com>
2024-08-08 12:33:45 +0530

    add the server start code in main root file app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,13 @@
 // this is the root file which will executed to start the node server
+
+const express=require('express');
+
+const app=express();
+
+
+app.use((req,res,next)=>{
+    console.log("server just started");
+    res.send("<h1>Welcome to Express server");
+})
+
+app.listen(3000);
