6d32879780e850c4a1d7563b2e5a0591de5f88ee
Prashanth Naroju <prashanth.naro@gmail.com>
2024-08-10 14:01:17 +0530

    store the admin added data of products as array in products

--- Backend/routes/view-products.js
+++ Backend/routes/view-products.js
@@ -7,15 +7,18 @@ const router=express.Router();
 
 const rootdir=require("../utils/utilpath");
 
+const products=[]
+
 router.get("/view-products",(req,res,next)=>{
     console.log("view products page rendered");
     res.sendFile(path.join(rootdir,"views",'view-products.html'));
 })
 
 router.post("/view-products",(req,res,next)=>{
-    console.log(req.body);
+    products.push(req.body);
+    console.log(products);
     res.redirect("/");
 })
 
-module.exports=router
+module.exports=router;
 
