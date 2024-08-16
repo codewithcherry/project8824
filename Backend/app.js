6552161230e9036c5e8a68ac698545be13aa1b22
Prashanth Naroju <prashanth.naro@gmail.com>
2024-08-16 11:06:25 +0530

    add the pug code  in root file to set the global pug file

--- Backend/app.js
+++ Backend/app.js
@@ -10,6 +10,9 @@ const bodyParser=require("body-parser");
 
 const app=express();
 
+app.set('view engine','pug');
+app.set('views',"./views");
+
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(express.static(path.join(__dirname,"public")));
