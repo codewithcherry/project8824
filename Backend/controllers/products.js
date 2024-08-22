5e5e1628a99106eeadb9a1a06d4d47a065c1f240
Prashanth Naroju <prashanth.naro@gmail.com>
2024-08-22 13:05:19 +0530

    code the product.js model to serve the related controller for post and get products routes

--- Backend/controllers/products.js
+++ Backend/controllers/products.js
@@ -1,17 +1,24 @@
 // this file is to write the code for controller all products related functions to manioulate the data into views and models
+
+const Product=require("../models/product")
+
+
 exports.addProductController=(req,res,next)=>{
     console.log("add product page rendered");
     res.render("add-product",{pagetitle:"Add Products"});
 }
 
-const products=[]
+
 
 exports.postProductController=(req,res,next)=>{
-    products.push(req.body);
+    const {title,description,price}=req.body;
+    const product=new Product(title,description,price);
+    product.save()
     res.redirect("/view-products");
 }
 
 exports.getProductController=(req,res,next)=>{
     console.log("view products page rendered");
+    const products=Product.fetchAll()
     res.render('view-products',{pagetitle:"View Products",prods:products});
 }
--- Backend/models/product.js
+++ Backend/models/product.js
@@ -1,1 +1,20 @@
 // this file is to code the model for products controller
+
+const products=[]
+
+class Product{
+    constructor(title,description,price){
+        this.title=title;
+        this.description=description;
+        this.price=price;
+    }
+
+    save() {
+        products.push(this);
+    }
+    static fetchAll(){
+        return products
+    }
+}
+
+module.exports= Product;
