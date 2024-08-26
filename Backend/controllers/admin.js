3b601b1768dd0029e42a7fb7b8eb456f6fdf82d7
Prashanth Naroju <prashanth.naro@gmail.com>
2024-08-26 14:20:49 +0530

    code refactor at admin routes and controller for admin tasks

--- Backend/controllers/admin.js
+++ Backend/controllers/admin.js
@@ -1,26 +1,13 @@
 // this file is to write the code for controller all products related functions to manioulate the data into views and models
 
-const Product=require("../models/product")
-
-
 exports.addProductController=(req,res,next)=>{
     console.log("add product page rendered");
     res.render("admin/add-product",{pagetitle:"Add Products"});
 }
-
-
 
 exports.postProductController=(req,res,next)=>{
     const {title,description,price}=req.body;
     const product=new Product(title,description,price);
     product.save()
     res.redirect("/");
-}
-
-exports.getProductController=(req,res,next)=>{
-    console.log("view products page rendered");
-    Product.fetchAll((products)=>{
-        res.render('shop/view-products',{pagetitle:"View Products",prods:products});
-    })
-   
 }
--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -1,8 +1,6 @@
 //this is the route file for the adding products into the shop app
 const express=require('express')
-const path=require('path');
 
-const rootdir = require('../utils/utilpath');
 
 const productsController=require('../controllers/admin')
 
