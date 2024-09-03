1f4fcd164fb47b31dddff5a30703fd551290835c
Prashanth Naroju <prashanth.naro@gmail.com>
2024-09-03 19:40:04 +0530

    code database mysql code into connecting to pool of mysql server

--- Backend/utils/db.js
+++ Backend/utils/db.js
@@ -1,6 +1,8 @@
-const db =require('mysql2');
+const db = require('mysql2');
 const dbCredentials=require('./dbcredentials');
 
 const dbpool=db.createPool(
     dbCredentials
 )
+
+module.exports=dbpool.promise();
