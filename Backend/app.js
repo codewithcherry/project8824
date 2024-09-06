09defc2d046623f2748656161abb91077a46be26
Prashanth Naroju <prashanth.naro@gmail.com>
2024-09-06 14:03:12 +0530

    code the createNewUser and findUserByid() methods in user model

--- Backend/app.js
+++ Backend/app.js
@@ -1,27 +1,35 @@
 // this is the root file which will executed to start the node server
-
 const express=require('express');
-
 const path=require('path');
-
 const {mongoClientConnect}=require("./utils/db.js")
 // const rootdir=require('../Backend/utils/utilpath') this code is not used because of template engine
-
 const bodyParser=require("body-parser");
-
 const _404ErrorController=require("./controllers/404controller.js")
+const User=require("./models/users.js");
 
 const app=express();
 
 app.set('view engine','pug');
 app.set('views',"./views");
 
-app.use(bodyParser.urlencoded({extended:false}));
-
-app.use(express.static(path.join(__dirname,"public")));
 
 const adminRouter=require('./routes/adminRoutes.js');
 const shopRouter=require("./routes/shopRoutes.js");
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,"public")));
+
+app.use((req,res,next)=>{
+    User.findUserbyId("66dab8b0bdb87e934d2593eb")
+    .then(user=>{
+        console.log(user);
+        req.user=user;
+        next();
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+})
 
 
 app.use(adminRouter);
--- Backend/models/users.js
+++ Backend/models/users.js
@@ -1,14 +1,29 @@
-const {getDb}=require('../utils/db')
+const {getDb}=require('../utils/db');
+const mongodb=require('mongodb');
 
 class User{
-    constructor(userid,email){
-        this.userid=userid;
+    constructor(userName,email){
+        this.userName=userName;
         this.email=email;
     }
 
     createNewUser(){
         let db=getDb()
         return db.collection("users").insertOne(this);
+    }
+
+    static findUserbyId(uid){
+        let db=getDb();
+
+        return db.collection("users").findOne({_id:new mongodb.ObjectId(uid)})
+                .then(user=>{
+                    console.log("user found");
+                    return user;
+                    
+                })
+                .catch(err=>{
+                    console.log(err);
+                })
     }
 }
 
