018e2bf0e3a1738557030e7f56e1961730d6c235
Prashanth Naroju <prashanth.naro@gmail.com>
2024-09-18 22:00:02 +0530

    code the model and schema for saving new user using mongoose

--- Backend/models/product.js
+++ Backend/models/product.js
@@ -1,3 +1,25 @@
+const mongoose=require('mongoose');
+
+const productSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    }
+})
+
+const Product=mongoose.model("Product",productSchema);
+
+module.exports=Product;
+
+
 // const Cart=require('./cart')
 // const mongodb=require('mongodb')
 // const {getDb}=require("../utils/db");
