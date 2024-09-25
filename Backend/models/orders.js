758e99d64b249b8212d156303b8bfad535a3a89b
Prashanth Naroju <prashanth.naro@gmail.com>
2024-09-25 13:58:57 +0530

    create and code orders model for checkout the cart

---
+++ Backend/models/orders.js
@@ -0,0 +1,33 @@
+const mongoose=require("mongoose");
+
+const Schema=mongoose.Schema;
+
+const orderSchema=new Schema({
+    user:{
+        userId:{
+            type:Schema.Types.ObjectId,
+            ref:"User",
+            required:true
+        },
+        name:{
+            type:String,
+            required:true
+        }
+    },
+    products:[
+        {
+            product:{
+                type:Object,
+                required:true
+            },
+            quantity:{
+                type:Number,
+                required:true
+            }
+        }
+    ]
+})
+
+const Order=mongoose.model("Order",orderSchema);
+
+module.exports=Order;
