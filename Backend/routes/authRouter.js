7791fce84833f0b2bd236b24fe84eb06754a2205
Prashanth Naroju <prashanth.naro@gmail.com>
2024-10-01 14:22:27 +0530

    add the route for thr /signup path

--- Backend/routes/authRouter.js
+++ Backend/routes/authRouter.js
@@ -3,6 +3,9 @@ const router=express.Router();
 
 const authController=require("../controllers/auth");
 
+
+router.get("/signup",authController.getSignup);
+
 router.get("/login",authController.getLogin);
 router.post('/login',authController.postLogin);
 router.get("/logout",authController.getLogout);
