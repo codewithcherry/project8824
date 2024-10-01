e1b78649c7b0b7e804b622f5673a7549a186cc2f
Prashanth Naroju <prashanth.naro@gmail.com>
2024-10-01 15:59:14 +0530

    add route to signup the new user

--- Backend/routes/authRouter.js
+++ Backend/routes/authRouter.js
@@ -5,6 +5,7 @@ const authController=require("../controllers/auth");
 
 
 router.get("/signup",authController.getSignup);
+router.post("/signup",authController.postSignup);
 
 router.get("/login",authController.getLogin);
 router.post('/login',authController.postLogin);
