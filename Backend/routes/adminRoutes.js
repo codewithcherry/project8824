7779253f4f3322044139c172181687ca734df841
Prashanth Naroju <prashanth.naro@gmail.com>
2024-10-02 13:12:49 +0530

    add the isAuth() middle ware to protect the routes at admin and shop level and redirect to login

--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -1,22 +1,22 @@
 //this is the route file for the adding products into the shop app
 const express=require('express')
-
+const isAuth=require("../middlewares/isAuth");
 
 const adminController=require('../controllers/admin')
 
 const router=express.Router();
 
-router.get("/add-product",adminController.addProductController);
+router.get("/add-product", isAuth , adminController.addProductController);
 
-router.get("/productslist",adminController.getadminProductslist);
+router.get("/productslist", isAuth , adminController.getadminProductslist);
 
-router.post("/view-products",adminController.postProductController);
+router.post("/view-products", isAuth ,adminController.postProductController);
 
-router.post("/productslist",adminController.postEditProductDetails);
+router.post("/productslist", isAuth ,adminController.postEditProductDetails);
 
-router.post("/admin/delete-product",adminController.postDeleteProduct);
+router.post("/admin/delete-product", isAuth ,adminController.postDeleteProduct);
 
-router.get("/admin/edit-product/:productID",adminController.getEditProduct);
+router.get("/admin/edit-product/:productID",isAuth,adminController.getEditProduct);
 
 
 module.exports=router;
--- Backend/routes/shopRoutes.js
+++ Backend/routes/shopRoutes.js
@@ -2,11 +2,14 @@
 
 const express=require('express');
 const path=require("path")
+const isAuth=require("../middlewares/isAuth");
 
 const router=express.Router();
 
 const adminController=require("../controllers/admin")
 const shopController=require('../controllers/shop')
+
+
 
 // const rootdir=require("../utils/utilpath"); using template engine doesnot require the root dir
 
@@ -16,18 +19,18 @@ router.get("/view-products",shopController.getProdducts);
 
 router.get('/home',shopController.getShopHome);
 
-router.get('/product/:productId',shopController.getProductDetails);
+router.get('/product/:productId', isAuth ,shopController.getProductDetails);
 
 
-router.get("/orders",shopController.getOrders);
+router.get("/orders", isAuth, shopController.getOrders);
 
-router.get('/cart',shopController.getShopCart);
+router.get('/cart', isAuth, shopController.getShopCart);
+ 
+router.post("/cart", isAuth, shopController.postShopCart);
 
-router.post("/cart",shopController.postShopCart);
-
-router.post("/cart-remove-product",shopController.postDeleteCartProduct);
-router.post("/orders",shopController.postCartToOrders);
-router.get('/orders',shopController.getOrders);
+router.post("/cart-remove-product", isAuth, shopController.postDeleteCartProduct);
+router.post("/orders", isAuth, shopController.postCartToOrders);
+router.get('/orders', isAuth, shopController.getOrders);
 
 module.exports=router
 
