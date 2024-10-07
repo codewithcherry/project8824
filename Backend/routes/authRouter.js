8f058dd0b57c1ef2cb85368148e83eb9941440c4
Prashanth Naroju <prashanth.naro@gmail.com>
2024-10-07 14:33:33 +0530

    add the validators code to check the email,password inputs

--- Backend/routes/authRouter.js
+++ Backend/routes/authRouter.js
@@ -3,9 +3,23 @@ const router=express.Router();
 
 const authController=require("../controllers/auth");
 
+const {body}=require("express-validator");
+
 
 router.get("/signup",authController.getSignup);
-router.post("/signup",authController.postSignup);
+
+router.post("/signup", [
+    body('email').isEmail().withMessage("Enter a valid email id!"),
+    body('password').isLength({ min: 6 }).withMessage("Password must have at least 6 characters"),
+    body('confirmPassword').custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error('Password and confirm password do not match');
+        }
+        return true;  // Return true if validation passed
+    })
+],
+authController.postSignup);
+
 
 router.get("/login",authController.getLogin);
 router.post('/login',authController.postLogin);
